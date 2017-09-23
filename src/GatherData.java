f8fe6df8afd465a2b2ded89913acaf406f5121e7
victorflorintsev <victorflorintsev1996@gmail.com>
2017-09-22 21:17:07 -0500

    oops, mislabelled

--- src/GatherData.java
+++ src/GatherData.java
@@ -1,6 +1,15 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 
 /*
 This class defines the action that happens when the sidebar button is clicked.
@@ -14,8 +23,36 @@ public class GatherData extends AnAction {
         /* Here is where the main code lies for what happens on a click.
         The AnActionEvent class gives you all you need about the project. */
         Project project = event.getProject();
+        PsiClass psiClass = getPsiClassFromContent(event);
         System.out.println("CLICK");
+        ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Run");
+        System.out.println();
+        // ^ create a debug point here and check out the information, see if you can pinpoint some data to read.
+        // don't worry if you can't understand any of the variables, there are utility classes we need to find
+        // that can extract the information for us.
 
-        // After we've made the Action we need to register the GatherData class in plugin.xml
+        // After we've made the Action we need to register the GatherData class in plugin.xml in resources
+    }
+    private PsiClass getPsiClassFromContent(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+
+        // find offset for what is at the caret cursor
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        // it would help to know what a PSI tree is,
+        // it's essentially a parser tree of what's
+        // in IntelliJ, has some other cool features
+        // that might be useful to know.
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+        // learn to use PsiTreeUtil, in the words of the webinar "very very useful for IJ plugin development"
+        if (psiClass == null) {
+            return null;
+        }
+        // up to here is what gets the dropdown option to show up
+        return psiClass;
     }
 }
